462308490ecf6c2a1e05a1dd462b949b2483c29b
黄玄 <huxpro@gmail.com>
2016-11-21 01:10:19 +0800

    [update attachments]

---
+++ README.md
@@ -0,0 +1,29 @@
+# **Service Worker 101** 
+
+### Working Offline and Instant Loading (@GDG DevFest 2016)
+
+TLDR; It covers lots of cool stuff about Service Worker!
+
+### [Watch Slides → ](https://huangxuan.me/sw-101-gdgdf/)
+
+<img src="https://huangxuan.me/sw-101-gdgdf/attach/qrcode.png" width="350" />
+
+### What is in `code/`
+
+- Hello World of Service Worker
+- Make your own Offline Dinosaurs
+- Stale/Fastest while revalidate
+
+
+
+### Notes  
+
+This slides is powered by [Yanshuo.io (演说.io)](http://yanshuo.io), a online software helping you create, store and share web slides. 
+
+`index.html` is the HTML source code exported from [Yanshuo.io](http://yanshuo.io), and many of its dependencis (js, css, fonts) are still linked to CDN of [Yanshuo.io](http://yanshuo.io). You can do any secondary development and host it by yourself.
+
+
+### License
+
+MIT  
+Copyright (c) 2016 Hux
--- code/stale-while-revalidate/sw.js
+++ code/stale-while-revalidate/sw.js
@@ -25,6 +25,20 @@ self.onactivate = (e) => {
   //e.waitUntil(clients.claim())
 }
 
+function cacheFirst(e, cached, fetched, fallback){
+  e.respondWith(
+    cached.then(res => res || fetched).catch(_ => fallback)
+  )
+}
+
+function fastest(e, cached, fetched, fallback){
+  e.respondWith(
+    Promise.race([fetched.catch(_ => cached), cached])
+      .then(resp => resp || fetched)
+      .catch(_ => fallback)
+  )
+}
+
 self.onfetch = (e) => {
   const cached = caches.match(e.request)
   const fallback = caches.match('offline.html')
@@ -32,19 +46,13 @@ self.onfetch = (e) => {
   const fetched = fetch(`${e.request.url}?${Date.now()}`)
   const fetchedCopy = fetched.then(_ => _.clone())
 
-  // Race for fullfilled one with fallback
-  // e.respondWith(
-  //   Promise.race([fetched.catch(_ => cached), cached])
-  //     .then(resp => resp || fetched)
-  //     .catch(_ => fallback)
-  // )
-
   // Cache First with fallback
-    e.respondWith(
-      cached.then(res => res || fetched).catch(_ => fallback)
-    )
+  //cacheFirst(e, cached, fetched, fallback)
 
-  // revalidate
+  // or a better one: fastest, let network and cache race
+  fastest(e, cached, fetched, fallback)
+
+  // revalidate part
   e.waitUntil(
     Promise.all([fetchedCopy, caches.open('runtime')])
       .then(([resp, cache]) => resp.ok && cache.put(e.request, resp))
